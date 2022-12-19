9f65aa07f874ed7b192dc5de32ed00dae00f5614
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2022-12-19 11:20:37 +0500

    19-12-2022

---
+++ 19-12-2022/index.js
@@ -0,0 +1,116 @@
+//Class Task 1 Solution
+
+// var data=["SSC","HSC","BCS","BS"]
+
+// for(var i=0;i<data.length;i++){
+//     document.write(i+")"+data[i]+"<br>")
+// }
+
+// var data=["SSC","HSC","BCS","BS"]
+// document.write("<ol>")
+// for(var i=0;i<data.length;i++){
+//     document.write("<li>)"+data[i]+"</li><br>")
+// }
+// document.write("</ol>")
+
+
+
+
+
+
+
+
+//Class Work
+
+
+
+// function greet(){
+//     var name=prompt("enter your name")
+//     document.write("Hello "+name)
+// }
+
+// greet()
+// greet()
+
+
+
+// let op=prompt("Enter what you want to do +, -, *, /")
+// let num1=+prompt("Enter first number")
+// let num2=+prompt("Enter second number")
+// function add(num1,num2){
+    
+//     let result=num1+num2
+//     document.write(result)
+// }
+
+// function subtract(num1,num2){ //Parameters
+    
+//     let result=num1-num2
+//     document.write(result)
+// }
+// function multiply(num1,num2){
+   
+//     let result=num1*num2
+//     document.write(result)
+// }
+// function divide(num1,num2){
+   
+//     let result=num1/num2
+//     document.write(result)
+// }
+
+// if(op==="+"){
+//     add(num1,num2)
+// }
+// else if(op==="-"){
+//     subtract(num1,num2)
+// }
+// else if(op==="*"){
+//     multiply(num1,num2)
+// }
+// else if(op==="/"){
+//     divide(num1,num2)
+// }
+// else{
+//     document.write("Invalid Input")
+// }
+
+
+// switch(op){
+//     case "+":
+//         add(num1,num2)
+//         break
+//     case "-":
+//         subtract(num1,num2)
+//         break
+//     case "*":
+//         multiply(num1,num2)
+//         break
+//     case "/":
+//         divide(num1,num2)
+//         break
+//     default:
+//         document.write("Invalid Input")
+//         break
+
+// }
+//() --> Paranthesis
+//{} --> Scope (Limiter and delimiter)
+
+// let names=["Sameer","Ali","Laiba"]
+// function display(studentNames){
+//     for(let i=0;i<studentNames.length;i++){
+//         document.write(studentNames[i]+"<br>")
+//     }
+// }
+
+// display(names)
+
+
+//Arrow Functions
+let add=()=>{
+    document.write("Iam add function")
+}
+add()
+
+
