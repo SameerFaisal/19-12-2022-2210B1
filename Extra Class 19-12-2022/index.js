fba7ecdfcc1a255ff57f3d23ad6b48ef613c849e
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2022-12-19 12:27:27 +0500

    Add files via upload

---
+++ Extra Class 19-12-2022/index.js
@@ -0,0 +1,100 @@
+// var num=25
+// do{
+//     document.write(num+"<br>")
+//     num++
+// }while(num>35)
+
+
+// let start=+prompt("Enter where you want to start the loop")
+// let end=+prompt("Enter where you want to end the loop")
+// for(let i=start;i<=end;i++){
+//     document.write(i+"<br>")
+// }
+
+
+// let number=+prompt("Enter a number")
+// if(number%2===0){
+//     document.write("Number is even")
+// }
+// else if(number%2!==0){
+//     document.write("Number is odd")
+// }
+// else{
+//     document.write("Input is not a number")
+// }
+
+// for(let i=0;i<=100;i++){
+//     if(i%2!==0){
+//              document.write(i+"<br>")
+//         }
+// }
+
+// let alphabet=prompt("Enter an alphabet")
+
+// if(alphabet==="a"||alphabet==="A"||alphabet==="e"||alphabet==="E"||alphabet==="i"||alphabet==="I"||alphabet==="o"||alphabet==="O"||alphabet==="u"||alphabet==="U"){
+//     document.write("You entered a vowel")
+// }
+// else{
+//     document.write("You entered a consonant")
+// }
+
+// let age=+prompt("Enter your age")
+// let city=prompt("Enter your city")
+
+// if(city==="Karachi"&&age===18){
+//     document.write("You are eligible to vote")
+// }
+// else if(city==="Karachi"&&age!==18){
+//     document.write("You are underage")
+// }
+// else if(city!=="Karachi"&&age===18){
+//     document.write("You are not from Karachi")
+// }
+// else{
+//     document.write("You are not eligible to vote")
+// }
+
+// let age=+prompt("Enter your age")
+// let city=prompt("Enter your city")
+
+// if(city==="Karachi"&&age>=18){
+//     document.write("You are eligible to vote")
+// }
+// else if(city==="Karachi"&&age<18){
+//     document.write("You are underage")
+// }
+// else if(city!=="Karachi"&&age>=18){
+//     document.write("You are not from Karachi")
+// }
+
+// else{
+//     document.write("You are not eligible to vote")
+// }
+
+// let number=+prompt("Enter a number")
+
+// switch(number%2===0){
+//     case true:
+//         document.write("Even Number")
+//         break
+//     case false:
+//         document.write("Odd Number")
+//         break
+//     default:
+//         document.write("invalid")
+// }
+
+// let alphabet=prompt("Enter an alphabet")
+
+// switch(alphabet){
+//     case "a"||"A"||"e"||"E"||"i"||"I"||"o"||"O"||"u"||"U":
+//         document.write("It is vowel")
+//         break
+//     default:
+//         document.write("Invalid input")
+//         break
+// }
+
+
+
+
